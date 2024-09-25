af374aa38ed015d8d21d208aacfd14b0dd70a83d
つづこ <>
2024-09-26 00:54:07 +0900

    関連付けの修正

--- db/migrate/20240923073353_create_boards.rb
+++ db/migrate/20240923073353_create_boards.rb
@@ -1,7 +1,7 @@
 class CreateBoards < ActiveRecord::Migration[7.2]
   def change
     create_table :boards do |t|
-      t.references :user, null: false
+      t.references :user
       t.string :title
       t.text :description
 
