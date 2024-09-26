8d4d22dd2f32bd88437c99eb1e1944b03f6ef89e
つづこ <>
2024-09-27 07:49:05 +0900

    紐づけなどの修正

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,9 +1,21 @@
 class BoardsController < ApplicationController
+  before_action :authenticate_user!
 
   def new
     @board = current_user.boards.build
   end
+
+  def create
+    @board = Board.new(board_params)
+    # if @board.save
+    #   redirect_to board_path(@board),
+    #               notice: '保存できました'
+    # else
+    #   flash.now[:error] = '保存に失敗しました'
+    #   render :new
+    # end
+  end
+
   def index
-    @board = Board.first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get '/' => 'boards#index'
+
   devise_for :users
+  resources :boards
+
+  root to: 'boards#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
